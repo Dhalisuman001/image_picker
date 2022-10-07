9939a2c592e3b1262e5161536cb17b917b6980dc
Suman Dhali <dhalisuman001@gmail.com>
2022-10-07 21:47:16 +0530

    for check

---
+++ android/app/src/main/kotlin/com/example/image__picker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.image__picker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/MyHomePage.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'dart:async';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Camera App"),
+      ),
+      body: Center(
+        child: _image == null ? Text("No Image.") : Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _optionDailogBox,
+        child: Icon(Icons.add_a_photo),
+        tooltip: "Open Camera",
+      ),
+    );
+  }
+
+  Future<void> _optionDailogBox() {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: Colors.blue,
+            shape: RoundedRectangleBorder(),
+            content: SingleChildScrollView(
+                child: ListBody(
+              children: [
+                GestureDetector(
+                  child: Text(
+                    "Take a picture",
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                  ),
+                  onTap: openCamera,
+                ),
+                Padding(padding: EdgeInsets.all(10.0)),
+                GestureDetector(
+                  child: Text(
+                    "Select a picture from gallery",
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                  ),
+                  onTap: openGallery,
+                )
+              ],
+            )),
+          );
+        });
+  }
+
+  Future openCamera() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _image = image;
+    });
+  }
+
+  Future openGallery() async {
+    var picture = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = picture;
+    });
+  }
+}
